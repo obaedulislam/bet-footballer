3e09f7e4ac8892d3e35c3a4ee059ffd524b5bacc
obaedulislam <obaed.cse@gmail.com>
2022-08-22 23:43:40 +0600

    Five player list added dynamically successfull

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
 /* Player Selection Button Event Listener */
 function addPlayer(element){
-    const
-    
-    
+    const fiveList = document.getElementById("add-player");
+    const items = fiveList.getElementsByTagName('li');
+    const item = items.length;
+    if(item < 5){
+        const li = document.createElement('li');
+        const playerName = element.parentNode.parentNode.children[0].innerText;
+        li.innerText = playerName;
+        fiveList.appendChild(li);
+        element.disabled = "true";
+    } 
+    else{
+        alert("You cant added more than five player")
+    }
 }
+
